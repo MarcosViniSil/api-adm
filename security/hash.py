7315d54fdf03fb81cdc855aed87acc876f472baa
MarcosViniSil <marcossilv203@gmail.com>
2026-03-16 10:43:47 -0300

    chore: adjust requirements.txt

--- security/hash.py
+++ security/hash.py
@@ -4,11 +4,9 @@ def createHashForPassword(password:str) -> str:
     print("password ",password)
     if password is None:
         raise ValueError(f"A senha não foi informada")
-    logging.info("password ",password)
     passwordBytes = password.encode("utf-8")
-    logging.info("passwordBytes ",passwordBytes)
     try:
-        passwordHash = bcrypt.hashpw(password, bcrypt.gensalt())
+        passwordHash = bcrypt.hashpw(passwordBytes, bcrypt.gensalt())
         return passwordHash
     except Exception as e:
         raise ValueError(f"Não foi possível gerar o hash da senha informada, tente novamente {e} [DEV]")
